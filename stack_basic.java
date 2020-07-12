286516e0304d0654693941057b1f6979981fe362
unknown <singh.dajinder13@gmail.com>
2020-07-12 23:25:11 +0530

    updated

---
+++ stack_basic.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class stack_basic{
+    public static void main(String[] args) throws Exception{
+        solve();
+    }
+
+    public static void solve(){
+        // duplicatebrackets();
+        validParenthesis();
+    }
+    public static void duplicatebrackets(){
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+
+            if(ch == ')'){
+                if(st.peek()=='('){
+                    System.out.println("true : duplicate brackets are present");
+                    return;
+                }else{
+                    while(st.peek()!='('){
+                        st.pop();
+                    }
+                    st.pop();
+                }
+                
+            }else{
+                st.push(ch);
+            }
+        }
+        System.out.println("false : duplicate brackets are not present");
+    }
+
+    public static void validParenthesis(){
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+
+        Stack<Character>st = new Stack<>();
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else if(ch == ')'){
+                if(st.size()==0){
+                    System.out.println(false);
+                    return;
+                }
+                else if(st.peek()!='('){
+                    System.out.println(false);
+                    return;
+                }
+                else{
+                    st.pop();
+                }
+            }
+            else if(ch == ']'){
+                if(st.size()==0){
+                    System.out.println(false);
+                    return;
+                }
+                else if(st.peek()!='['){
+                    System.out.println(false);
+                    return;
+                }
+                else{
+                    st.pop();
+                }
+            }
+            else if(ch == '}'){
+                if(st.size()==0){
+                    System.out.println(false);
+                    return;
+                }
+                else if(st.peek()!='{'){
+                    System.out.println(false);
+                    return;
+                }
+                else{
+                    st.pop();
+                }
+            }
+            else{
+
+            } 
+        }
+        if(st.size()==0){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }   
+
+}
