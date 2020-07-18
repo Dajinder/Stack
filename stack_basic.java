dd29823b86423978cd9dfdb76b4613e700bb4fa2
unknown <singh.dajinder13@gmail.com>
2020-07-18 17:39:01 +0530

    updated

--- stack_basic.java
+++ stack_basic.java
@@ -8,7 +8,8 @@ public class stack_basic{
     public static void solve(){
         // duplicatebrackets();
         // validParenthesis();
-        addDuplicate();
+        // addDuplicate();
+        // ngor();
     }
     public static void duplicatebrackets(){
         Scanner scn = new Scanner(System.in);
@@ -104,15 +105,52 @@ public class stack_basic{
         Stack<Character> st = new Stack<>();
         for(int i=0;i<str.length();i++){
             char ch = str.charAt(i);
-            if(ch == st.peek() && st.size()!=0){
+            if(st.size()==0){
+                st.push(ch);
+            }
+            if(ch == st.peek()){
                 st.pop();
-            }else{
+            }
+            else{
                 st.push(ch);
             }
         }
-        while(st.size()!=0){
-            System.out.println(st.pop());
+        int size = st.size();
+        while(size!=0){
+            char x = st.peek();
+            System.out.println(x);
+            st.pop();
+            size--;
         }
     }
 
+    public static void ngor(){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = scn.nextInt();
+        }
+
+        int[] nge = new int[arr.length];
+        Stack<Integer>st = new Stack<>();
+        nge[arr.length-1] = -1;
+        st.push(arr[arr.length-1]);
+
+        for(int i=arr.length-2;i>=0;i--){
+            while(st.size()>0 && arr[i]>=st.peek()){
+                st.pop();
+            }
+
+            if(st.size()==0){
+                nge[i] = -1;
+            }else{
+                nge[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+        for(int i=0;i<nge.length;i++){
+            System.out.println(nge[i]);
+        }
+    }
 }
