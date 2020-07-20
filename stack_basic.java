89a9eb9660f7948893389b6e21c07402800b1af9
unknown <singh.dajinder13@gmail.com>
2020-07-20 22:37:17 +0530

    updated

--- stack_basic.java
+++ stack_basic.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Arrays;
 public class stack_basic{
     public static void main(String[] args) throws Exception{
         solve();
@@ -10,6 +11,13 @@ public class stack_basic{
         // validParenthesis();
         // addDuplicate();
         // ngor();
+        
+        
+        // int[] arr = {2,5,9,3,1,12,6,8,7};
+        // ngor_alternate(arr);
+        // nsor(arr);
+        // ngol(arr);
+        // nsol(arr);
     }
     public static void duplicatebrackets(){
         Scanner scn = new Scanner(System.in);
@@ -124,6 +132,10 @@ public class stack_basic{
         }
     }
 
+    // ngor = next greater on right
+    // nsor = next smaller on right
+    // ngol = next greater on left
+    // nsol = next smaller on left
     public static void ngor(){
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
@@ -151,6 +163,103 @@ public class stack_basic{
         }
         for(int i=0;i<nge.length;i++){
             System.out.println(nge[i]);
+        }
+    }
+
+    public static void ngor_alternate(int[] arr){
+        Stack<Integer>st = new Stack<>();
+        int[] ans = new int[arr.length];
+        st.push(0);
+        for(int i=1;i<arr.length;i++){
+            while(st.size()>0 && arr[i]>arr[st.peek()]){
+                int pos = st.peek();
+                ans[pos] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+
+        }
+
+        while(st.size()>0){
+            int pos = st.peek();
+            ans[pos] = -1;
+            st.pop();
+        }
+
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+
+    public static void nsor(int[] arr){
+        int[] ans = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        for(int i=0;i<arr.length;i++){
+            while(st.size()!=0 && arr[i]<arr[st.peek()]){
+                int pos = st.peek();
+                ans[pos] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        while(st.size()!=0){
+            int pos = st.peek();
+            ans[pos] = -1;
+            st.pop();
+        }
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+
+
+    public static void ngol(int[] arr){
+        Stack<Integer>st = new Stack<>();
+        int[] ans = new int[arr.length];
+        st.push(arr.length-1);
+        for(int i=arr.length-2;i>=0;i--){
+            while(st.size()!=0 && arr[i]>arr[st.peek()]){
+                int pos = st.peek();
+                ans[pos] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        while(st.size()!=0){
+            int pos = st.peek();
+            ans[pos] = -1;
+            st.pop();
+        }
+
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+
+
+    public static void nsol(int[] arr){
+        Stack<Integer>st = new Stack<>();
+        int[] ans = new int[arr.length];
+        st.push(arr.length-1);
+        for(int i=arr.length-2;i>=0;i--){
+            while(st.size()!=0 && arr[i] < arr[st.peek()]){
+                int pos = st.peek();
+                ans[pos] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        while(st.size()!=0){
+            int pos = st.peek();
+            ans[pos] = -1;
+            st.pop();
+        }
+
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
         }
     }
 }
